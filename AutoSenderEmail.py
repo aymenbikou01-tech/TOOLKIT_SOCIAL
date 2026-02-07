506e5f61a9bf69fd426eb4dde2b709f4dd5b639b
404 <aymenbikou01@gmail.com>
2026-02-07 21:02:30 +0100

    AutoSenderEmail script for sending HTML emails
    This script sends an email with HTML content using Gmail's SMTP server. It prompts for the receiver's email and the HTML file to use as the message body.

---
+++ AutoSenderEmail.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from colorama import Fore, Style, init
+from email.mime.text import MIMEText
+from pyfiglet import Figlet
+
+init()
+
+f = Figlet(font='slant')
+print(Fore.CYAN + f.renderText('SendEmail-404') + Style.RESET_ALL)
+
+
+SENDER_EMAIL = "i#####@gmail.com"#your emial phishing
+APP_PASSWORD = "##pppo09709##"#app pass
+RECEIVER_EMAIL = input("emial phishin: ")
+HTML_FILE = input('Message file html:')
+
+
+with open(HTML_FILE, "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+msg = MIMEMultipart("alternative")
+msg["Subject"] = "pour vous"
+msg["From"] = SENDER_EMAIL
+msg["To"] = RECEIVER_EMAIL
+
+msg.attach(MIMEText(html_content, "html"))
+
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+    server.login(SENDER_EMAIL, APP_PASSWORD)
+    server.send_message(msg)
+
+print("[+] Attack started ....")
